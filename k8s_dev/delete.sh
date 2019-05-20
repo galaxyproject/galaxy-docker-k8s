071e8a1bfa650e9d647f02aeb9f774018078f42f
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2019-05-20 10:55:12 -0400

    Cleanup (delete old k8s_dev files)

--- k8s_dev/delete.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-kubectl delete service postgres-service && \
-kubectl delete deploy --all && \
-kubectl delete configmap --all && \
-kubectl delete pvc --all && \
-kubectl delete pv --all
