8e86a30f307071da96e25a5b82fcb3d2e9ec381f
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2019-05-23 12:05:23 -0400

    Wrap psql command in bash (galaxy-init script)

--- scripts/pg-galaxy-init.sh
+++ scripts/pg-galaxy-init.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
 # Creates galaxy user+db, updates privileges/ownership in a new database inside postgres container
-# Required argument: port number. To get the port number, run `docker ps`:
-# > docker ps
-#CONTAINER ID        IMAGE               COMMAND                  CREATED             STATUS              PORTS                     NAMES
-#0ba2d68af1de        postgres:10.6       "docker-entrypoint.s…"   35 minutes ago      Up 35 minutes       0.0.0.0:32772->5432/tcp   gpsql
-#
-#In the above output, 32772 is the port number.
 
-USAGE="Usage: pg-galaxy-init.sh port (see comments in file)"
-PORT=${1:?"${USAGE}"}
-
-set -e
-
-psql -h localhost -p $1 -U postgres -v ON_ERROR_STOP=1 <<-EOSQL
+docker exec -i gpsql \
+psql -U postgres -v ON_ERROR_STOP=1 <<EOSQL
 		CREATE DATABASE galaxy;
 		CREATE USER galaxydbuser;
 		ALTER ROLE galaxydbuser WITH PASSWORD '42';
